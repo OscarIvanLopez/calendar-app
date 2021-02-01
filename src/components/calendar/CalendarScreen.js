0503c059679ec97e125416ef5d0ebd2038236d9a
Oscar Ivan <oscari.lopez@potros.itson.edu.mx>
2021-02-01 00:02:50 -0800

    01-feb-2021

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import Navbar from "../ui/Navbar";
+import CalendarEvent from "./CalendarEvent";
+import CalendarModal from "./CalendarModal";
+import { messages } from "../../helpers/calendar-messages-es";
+
+
+import "moment/locale/es";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+moment.locale("es");
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños de Aydin<3",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fff",
+    notes: "Comprar el pastel",
+    user: {
+      id: "123",
+      name: "Ivan",
+    },
+  },
+];
+
+const CalendarScreen = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "month"
+  );
+
+  // Event functions
+  const onDoubleClick = (e) => {
+    console.log(e);
+  };
+  const onSelectEvent = (e) => {
+    console.log(e);
+  };
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem("lastView", e);
+  };
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundcolor: "#367CF7",
+      bordarRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+    return {
+      style,
+    };
+  };
+  return (
+    <div className="calendar-screen">
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{
+          event: CalendarEvent,
+        }}
+      />
+
+      <CalendarModal />
+    </div>
+  );
+};
+
+export default CalendarScreen;
